82ad08dd0fc4948ff36d0465d211f6c07d5fbb8d
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-01 17:41:41 +0400

    interface

---
+++ src/Inheritance/Dog.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Dog extends Animal {
+    public void gav() {
+        System.out.println("Я умею гавкать");
+    }
+
+    public void eat() {
+        System.out.println("Собака кушает");
+    }
+}
---
+++ src/Inheritance/Main.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.eat();
+        dog.eat();
+    }
+}
