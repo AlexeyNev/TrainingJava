9137ef615528fc98ee983b431d2c4e7bab93a4a5
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-02 17:20:33 +0400

    polymorf

--- src/Interfaces/Test.java
+++ src/Interfaces/Test.java
@@ -2,11 +2,8 @@ package Interfaces;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal1 = new Animal(1);
-        Person person1 = new Person("Bob");
-
-        animal1.showInfo();
-        person1.showInfo();
+        Info info = new Animal(1);
+        Info info1 = new Person("Bob");
 
     }
 }
---
+++ src/Polymorf/Dog.java
@@ -0,0 +1,12 @@
+package Polymorf;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("Dog is bark");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+}
