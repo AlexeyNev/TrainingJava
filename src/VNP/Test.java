be26bc04924378b815bd188c5c4ff7919ba4890b
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-03 16:59:41 +0400

    Upward and downward transformation.

--- src/VNP/Test.java
+++ src/VNP/Test.java
@@ -2,12 +2,12 @@ package VNP;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal = new Dog(); // Upcasting - восходящее преобразование, тк dog стоит ниже класса animal
-        Dog dog = new Dog();
+        Animal a = new Animal();
+        Dog dog = (Dog) a;
+        dog.bark();
 
-        Animal animal1 = dog;
-
-        Dog dog1 = (Dog) animal1; // Downcasting - нисходящее преобразование
-        dog1.bark();
+        /**
+         * Восходящее преобразование безопасно, а нисходящее наоборот.
+         */
     }
 }
