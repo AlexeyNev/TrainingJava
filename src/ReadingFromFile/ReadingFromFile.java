2760ca3e0b2c484ff113b9c4ccc506aaa877cde9
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-04 17:11:47 +0400

    ReadingFromFile

---
+++ src/ReadingFromFile/ReadingFromFile.java
@@ -0,0 +1,21 @@
+package ReadingFromFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadingFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = separator + "C:" + separator + "Users" + separator + "Алексей" + separator + "Desktop" + separator + "file.txt";
+
+
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+    }
+}
