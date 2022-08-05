696e0d47143efd7511cd3ac055febb66dc8fc89c
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-05 19:11:57 +0400

    Throwing exceptions 2.

--- src/Exception2/Exception2.java
+++ src/Exception2/Exception2.java
@@ -4,17 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Exception2 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws ScannerException {
         Scanner scanner = new Scanner(System.in);
         while (true) {
            int x = Integer.parseInt(scanner.nextLine());
 
            if (x != 0) {
-               try {
-                   throw new IOException();
-               } catch (IOException e) {
-                   System.out.println("Пользователь ввел что-то кроме 0");
-               }
+               throw new ScannerException("Пользователь ввел что-то кроме 0");
            }
         }
     }
