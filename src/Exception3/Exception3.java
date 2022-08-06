3596845c5a69ea58303392df294e240493839d10
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-06 10:46:12 +0400

    Checked and Unchecked exceptions.

--- src/Exception3/Exception3.java
+++ src/Exception3/Exception3.java
@@ -5,10 +5,17 @@ package Exception3;
  * в Java все исключения деляться на два типа - Checked и Unchecked.
  * Checked (Compile time exception) - те исключения, которые возникают во время компиляции. = исключительные случаи
  * в работе программы.
- * Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы. =
+ * Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы. = ошибка в
+ * работе программы.
  */
 public class Exception3 {
     public static void main(String[] args) {
-        int s = 1/0; // Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы.
+//        int s = 1/0; // Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы.
+
+//        String name = null;
+//        name.length();
+
+//          int[] arr = new int[2];
+//        System.out.println(arr[2]);
     }
 }
