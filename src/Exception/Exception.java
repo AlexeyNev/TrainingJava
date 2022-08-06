ebad8e9823808ca7a34250fa0ff2afadc22bd26d
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-06 10:41:30 +0400

    Checked and Unchecked exceptions.

--- src/Exception/Exception.java
+++ src/Exception/Exception.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Exceptions {
+public class Exception {
 
     /* public static void main(String[] args) throws FileNotFoundException {
         readFile();
---
+++ src/Exception3/Exception3.java
@@ -0,0 +1,14 @@
+package Exception3;
+
+
+/**
+ * в Java все исключения деляться на два типа - Checked и Unchecked.
+ * Checked (Compile time exception) - те исключения, которые возникают во время компиляции. = исключительные случаи
+ * в работе программы.
+ * Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы. =
+ */
+public class Exception3 {
+    public static void main(String[] args) {
+        int s = 1/0; // Unchecked (Runtime exception) - те исключения, которые возникают во время выполнения программы.
+    }
+}
