8ac99d2040bffa9acf857f8d257eb07e0626bd03
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-18 12:25:43 +0400

    Overload

--- src/Overload/Employ.java
+++ src/Overload/Employ.java
@@ -2,15 +2,20 @@ package Overload;
 
 public class Employ {
 
-    Employ(int id1, String surname1, int age1) {
-        id = id1;
-        surname = surname1;
-        age = age1;
+    Employ(int id2, String surname2, int age2) {
+        this(id2, surname2, age2, 0.0, null);
     }
 
-    Employ(String surname1, int age1) {
-        surname = surname1;
-        age = age1;
+    Employ(String surname3, int age3) {
+        this(0,"", 3, 0.0, null);
+    }
+
+    Employ(int id4, String surname4, int age4, double salary4, String department4) {
+        id = id4;
+        surname = surname4;
+        age = age4;
+        salary = salary4;
+        department = department4;
     }
 
     int id;
@@ -26,5 +31,7 @@ class EmployTest {
         System.out.println(employ.age);
         Employ employ1 = new Employ("Katkov", 21);
         System.out.println(employ1.surname);
+        Employ employ2 = new Employ(25, "Sidorov", 4, 2.3, "eee");
+        System.out.println(employ2.department);
     }
 }
