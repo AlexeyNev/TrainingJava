bf1627baaab3e2a61fbb5ce2a3aba343905e317f
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-27 15:05:14 +0400

    Overload

--- src/Overload/Employ.java
+++ src/Overload/Employ.java
@@ -17,6 +17,11 @@ public class Employ {
         salary = salary4;
         department = department4;
     }
+    Employ(int id5, String surname5, double salary5) {
+        id = id5;
+        surname = surname5;
+        salary = salary5;
+    }
 
     int id;
     String surname;
@@ -33,5 +38,7 @@ class EmployTest {
         System.out.println(employ1.surname);
         Employ employ2 = new Employ(25, "Sidorov", 4, 2.3, "eee");
         System.out.println(employ2.department);
+        Employ employ3 = new Employ(2, "Terentev", 2.4);
+        System.out.println(employ3.surname);
     }
 }
