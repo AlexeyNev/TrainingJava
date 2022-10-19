cfb2f90ca6e488ce5e8e1a460ace53e8759a54dd
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-10-19 16:03:30 +0400

    Queue and LinkedList

---
+++ src/Queue/QueueLinkedListEx.java
@@ -0,0 +1,20 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueLinkedListEx {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Alex");
+        queue.add("Petr");
+        queue.add("Mark");
+        queue.add("Marsic");
+        queue.add("Alla");
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue);
+        System.out.println(queue.element());
+        System.out.println(queue);
+    }
+}
