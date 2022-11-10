1353f0ea08888832fbf434291712a191091b66bd
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-10 13:49:06 +0400

    2.0. Function. Возвести число в квадрат

---
+++ src/Lambda/test/FunctionPow.java
@@ -0,0 +1,15 @@
+package Lambda.test;
+
+import java.util.function.Function;
+
+public class FunctionPow {
+
+    public static double calculate(double x) {
+         return calculate(n -> n * n, x);
+    }
+
+    public static double calculate(Function<Double, Double> y, double x) {
+        return y.apply(x);
+    }
+
+}
