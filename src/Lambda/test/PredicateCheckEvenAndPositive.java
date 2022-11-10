1a5d443927092c5e5d66a06b53358a849661fd0e
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-10 13:47:25 +0400

    1.2. Predicate. Проверить, что число положительное и четное

---
+++ src/Lambda/test/PredicateCheckEvenAndPositive.java
@@ -0,0 +1,14 @@
+package Lambda.test;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckEvenAndPositive {
+
+    public static boolean check(int num) {
+         return check(n -> (n % 2 == 0 && n > 0), num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
