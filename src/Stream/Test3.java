b15d3cb5a963592a984633f95161a87f679c9a59
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-14 15:44:35 +0400

    Stream/forEach()

---
+++ src/Stream/Test3.java
@@ -0,0 +1,18 @@
+package Stream;
+
+import java.util.Arrays;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int [] array = {5, 9 , 4, 3, 1};
+//        Arrays.stream(array).forEach(e -> System.out.println(e));
+        Arrays.stream(array).forEach(Utils::myMethod);
+    }
+}
+
+class Utils {
+    public static void myMethod(int a) {
+        a = a + 5;
+        System.out.println("Element = " + a);
+    }
+}
