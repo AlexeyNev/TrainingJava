3a228f703a1847446e80f005c4b33573595c627f
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-16 16:17:51 +0400

    1.6. Создание стрима примитивов. Метод Arrays.stream()

---
+++ src/StreamTraining/ArraysStreamForPrimitive.java
@@ -0,0 +1,22 @@
+package StreamTraining;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Класс Stream работает с объектами.
+ * Для представления стрима примитивов в Java есть свои классы: IntStream, LongStream, DoubleStream.
+ * Их можно создавать как мы создавали раньше.
+ * Чаще всего приходиться создавать стрим из массива примитивов.
+ * Для этого лучше всего подходит метод Arrays.stream().
+ * Например: int[] data = {1, 2, 3};
+ * IntStream steam = Arrays.stream(data);
+ *
+ * Ваша задача создать стрим из переданного массива
+ */
+
+public class ArraysStreamForPrimitive {
+    public static IntStream createStream(int[] data) {
+         return Arrays.stream(data);
+    }
+}
