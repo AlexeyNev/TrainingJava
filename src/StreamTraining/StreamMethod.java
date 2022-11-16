dfc962c596401eac32737a17c4dd7c71dfd17a2e
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-16 15:24:06 +0400

    1.1. Создание стрима. Метод stream()

---
+++ src/StreamTraining/StreamMethod.java
@@ -0,0 +1,10 @@
+package StreamTraining;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class StreamMethod {
+    public static Stream<Integer> createStream(Collection<Integer> data) {
+        return data.stream();
+    }
+}
