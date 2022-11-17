f59a55367fbe30f1ae9c718637013edc860774d9
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-17 15:26:13 +0400

    2.4. Понятие редукции. Метод min(). Поиск кратчайшей строки

---
+++ src/StreamTraining/MinMethod.java
@@ -0,0 +1,23 @@
+package StreamTraining;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Как вы наверное уже догадались поиск минимального элемента представляет собой также редукцию.
+ * Этот метод принимает Comparator и возвращает Optional.
+ * Если список пуст вернется пустой Optional.
+ *
+ * Пример:
+ * Integer min = List.of(3, 2, 1).stream().min((n1, n2) -> Integer.compare(n1, n2)).get(); // вернет 1
+ *
+ * Ваша задача найти строку с минимальной длиной. Размер списка больше 0
+ */
+
+public class MinMethod {
+    public static String min(List<String> list) {
+        return list.stream()
+                .min(Comparator.comparingInt(String::length))
+                .get();
+    }
+}
