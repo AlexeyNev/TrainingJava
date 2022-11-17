154e5f9fb7bbd0ec3601ac05764e9bf2525ebf5d
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-17 15:39:31 +0400

    3.2. Сборка элементов. Сборка в Map

---
+++ src/StreamTraining/CollectToMap.java
@@ -0,0 +1,25 @@
+package StreamTraining;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Для того чтобы собрать данные в мапу, мы можем использовать методы collect() и Collectors.toMap().
+ * Сборка идет в HashMap
+ *
+ * Например:
+ * Map<Integer, Integer> result = Set.of(1, 2).stream().collect(Collectors.toMap(k -> k, v -> v));
+ * В качестве первого аргумента мы указываем,
+ * как мы будет строить ключ из элемента стрима,
+ * а в качестве второго как будем строить значение.
+ * В примере, ключом и значением будет сам элемент.
+ *
+ * Ваша задача собрать, переданный стрим чисел в мапу, где ключ сам элемент, а значение это его квадрат
+ */
+
+public class CollectToMap {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toMap(key -> key, value ->  value * value));
+    }
+}
