d076be5949712abb9c58b69db14eff19957934f5
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-22 18:15:03 +0400

    6.18. Промежуточные операции. Метод dropWhile()

---
+++ src/StreamTraining/DropWhileMethod.java
@@ -0,0 +1,23 @@
+package StreamTraining;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Данный метод похож на метод takeWhile() только он позволяет пропустить элементы, которые удовлетворяют условию.
+ * Этот метод также нужно использовать только для отсортированных элементов,
+ * т.к. если для первого элемента не выполниться условие, то в стриме останутся все элементы.
+ *
+ * Ваша задача оставить в списке только положительные элементы.
+ * Порядок элементов исходного списка может быть произвольным
+ */
+
+public class DropWhileMethod {
+    public static List<Integer> dropWhile(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .sorted()
+                .dropWhile(e -> e < 0)
+                .collect(Collectors.toList());
+    }
+}
