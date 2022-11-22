2930025f27b292dc97385d52cf3adb32090e9b2b
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-22 13:50:54 +0400

    6.12. Промежуточные операции. Метод limit()

---
+++ src/StreamTraining/LimitMethod.java
@@ -0,0 +1,22 @@
+package StreamTraining;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Чтобы взять какое-то число первых элементов стрима, можно использовать метод limit()
+ *
+ * Например:
+ *  List.of(1, 2, 3, 4, 5).stream().limit(2) // будет стрим из элементов 1, 2
+ *
+ * Ваша задача взять первые 3 элемента из списка
+ */
+
+public class LimitMethod {
+    public static List<String> firstThree(List<String> strings) {
+        return strings
+                .stream()
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+}
