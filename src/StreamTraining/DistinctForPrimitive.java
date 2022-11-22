9f63fd9df3f8055d1c5585bc80825253f4c68c09
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-22 13:59:32 +0400

    6.14. Промежуточные операции. Метод distinct(). Уникальные числа

---
+++ src/StreamTraining/DistinctForPrimitive.java
@@ -0,0 +1,24 @@
+package StreamTraining;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Для того, чтобы получить стрим из уникальных элементов мы можем использовать метод distinct().
+ * Если мы используем стрим примитивов, то сравнение элементов идет через ==, если стрим объектов, то через метод equals.
+ *
+ * Например:
+ * Arrays.stream(new int[] {1, 2, 1}).distinct() // в стриме останутся только 1, 2
+ *
+ * Ваша задача собрать уникальные числа массива в список
+ */
+
+public class DistinctForPrimitive {
+    public static List<Integer> collect(int[] data) {
+        return Arrays.stream(data)
+                .distinct()
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
