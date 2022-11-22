e0a9929cf80e94112bf5d54c8a3be9e55efddc53
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-22 14:18:29 +0400

    6.17. Промежуточные операции. Метод sorted() с компаратором.

---
+++ src/StreamTraining/SortedWithComparator.java
@@ -0,0 +1,21 @@
+package StreamTraining;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Метод sorted() может принимать также Comparator.
+ *
+ * Ваша задача отсортировать строки по убыванию.
+ * Нужно использовать Comparator.reverseOrder() для получения компаратора
+ */
+
+public class SortedWithComparator {
+    public static List<String> sorted(List<String> strings) {
+        return strings
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+}
