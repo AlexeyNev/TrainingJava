1bf36281bc3bd7aa2e468860242f1c9e94c7b0d1
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-23 16:16:12 +0400

    5.1. Optional. Методы isPresent(), isEmpty(), get()

---
+++ src/StreamTraining/OptionalGetAndIsPresent.java
@@ -0,0 +1,48 @@
+package StreamTraining;
+
+import java.util.Optional;
+
+/**
+ * Для того, чтобы проверить, что в Optional есть значение или нет, можно использовать метод isPresent(), isEmpty().
+ *
+ * Например:
+ * Optional.of(1).isPresent(); // даст true
+ * Optional.empty().isEmpty(); // даст true
+ *
+ * Для получения значения, которое мы поместили с помощью метода of() мы можем использовать метод get()
+ *
+ * Например:
+ * Optional.of(1).get() // Вернет 1
+ * Optilnal.empty().get() // Вернет исключение!
+ *
+ * Запомните, перед вызовом метода get() всегда нужно использовать isPresent().
+ * В этом суть Optional, чтобы программист не забывал делать проверки.
+ *
+ * Ваша задача:
+ *
+ * 1. Написать метод indexOf(). Он ищет индекс по значению. Если индекс не найден,
+ * должно вернуться Optional.empty(). Если найден, то Optional из этого индекса
+ * 2. В методе get() нужно вызвать метод indexOf().
+ * Если вернулся пустой Optinal, то метод должен вернуть -1, в противном случае сам индекс
+ */
+
+public class OptionalGetAndIsPresent {
+
+    public static int get(int[] data, int el) {
+        Optional<Integer> rsl = indexOf(data, el);
+        if (rsl.isEmpty()) {
+            rsl = Optional.of(-1);
+        }
+        return rsl.get();
+    }
+
+    private static Optional<Integer> indexOf(int[] data, int el) {
+        Optional<Integer> optional = Optional.empty();
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                optional = Optional.of(i);
+            }
+        }
+        return optional ;
+    }
+}
