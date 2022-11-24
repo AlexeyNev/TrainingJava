7d5ea6c35eaa35afdcdf73a9d9ceb464e3f950af
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-24 17:01:10 +0400

    Multithreading/getPriority() getName()

---
+++ src/Multithreading/Example5.java
@@ -0,0 +1,19 @@
+package Multithreading;
+
+public class Example5 {
+    public static void main(String[] args) {
+        MyThread5 myThread5 = new MyThread5();
+        myThread5.setName("my potok");
+        myThread5.setPriority(Thread.MIN_PRIORITY);
+        System.out.println("Name of myThread5 = " + myThread5.getName() +
+             " Priority of myThread5 = " + myThread5.getPriority());
+
+    }
+}
+
+class MyThread5 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Hello");
+    }
+}
