0bff24a4be243107946dbb2d0fc62237b13b0788
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-30 14:41:20 +0400

    FileWriter/Заполнения файла без указания пути

--- src/Work_with_files/FileWriterEx.java
+++ src/Work_with_files/FileWriterEx.java
@@ -9,17 +9,22 @@ import java.io.IOException;
 
 public class FileWriterEx {
     public static void main(String[] args) throws IOException {
-        String rubai = " четверостишие; форма лирической поэзии, \n" +
-                " широко распространённая на Ближнем и Среднем Востоке (наравне с газелью и касыдой).\n" +
-                " Истоки лежат в устном народном творчестве иранцев и таджиков.\n";
+        String rubai1 = " четверостишие; форма лирической поэзии, \n" +
+                " широко распространённая на Ближнем и Среднем Востоке (наравне с газелью и касыдой).\n";
+
+        String rubai2 = " Истоки лежат в устном народном творчестве иранцев и таджиков.\n";
 
         FileWriter writer = null;
         try {
-            writer = new FileWriter("C:\\Users\\Алексей\\Desktop\\test1.txt");
+            writer = new FileWriter("test.txt", true);
 //            for (int i = 0; i < rubai.length(); i++) {
 //                writer.write(rubai.charAt(i));
 //            }
-            writer.write(rubai); // альтернативное заполнение файла без цикла
+              writer.write(rubai1); // альтернативное заполнение файла без цикла
+
+//              writer.write(s); // если так сделать - файл рубаи перезапишется. нужно добавить true в new FileWriter
+
+                writer.write(rubai2);
 
             System.out.println("Done!");
         } catch (IOException e) {
