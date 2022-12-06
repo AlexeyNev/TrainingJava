ec1a3c71507a50c0f2451e041941dc559ba4abc3
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-06 14:14:45 +0400

    Regex

--- src/Regex/Ragex2.java
+++ src/Regex/Ragex2.java
@@ -8,13 +8,27 @@ public class Ragex2 {
 //        String s1 = "ABCD ABCE ABCFABCGABCH"; //есть строка
 //        Pattern pattern1 = Pattern.compile("ABC"); //создаем паттерн, который с помощью compile() будет искать "ABCD"
 
-        String s1 = "ABDOP"; //есть строка
-        Pattern pattern1 = Pattern.compile("AB[C-F]OP");
+//        Matcher matcher = pattern1.matcher(s1); // сравниваем паттерн1 и s1 на соответсвие с помощью объекта matcher
+//        while (matcher.find()) { //через цикл с помощью find() ищем есть ли совпадения
+//        System.out.println("Position: " + matcher.start() + " " + matcher.group()); // и если есть - выводим их с помощью group()
 
-        Matcher matcher = pattern1.matcher(s1); // сравниваем паттерн1 и s1 на соответсвие с помощью объекта matcher
+//        String s1 = "ABDOP";
+//        Pattern pattern1 = Pattern.compile("AB[C-F]OP");
 
-        while (matcher.find()) { //через цикл с помощью find() ищем есть ли совпадения
-            System.out.println("Position: " + matcher.start() + " " + matcher.group()); // и если есть - выводим их с помощью group()
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc.");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("6abch$");
+
+        String s1 = "abcd abce abc5abcg6abch";
+        Pattern pattern1 = Pattern.compile("\\d");
+
+
+        Matcher matcher = pattern1.matcher(s1);
+
+        while (matcher.find()) {
+            System.out.println("Position: " + matcher.start() + " " + matcher.group());
         }
     }
 }
