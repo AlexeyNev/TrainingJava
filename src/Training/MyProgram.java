07ccb3a492b5182a370ebd2430b392afa8ab3b2c
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-27 16:01:35 +0400

    training

--- src/Training/MyProgram.java
+++ src/Training/MyProgram.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 class MyProgram{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(str.length());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == 0 && b == 0) {
+            System.out.println("inf");
+        } else {
+            System.out.println(a != 0 ? "1" : "0");
+        }
 
         sc.close();
     }
