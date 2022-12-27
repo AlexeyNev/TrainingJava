4a2d593bb4f82ca43763b2f47a6443bfd218b3d2
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-27 16:06:48 +0400

    training

--- src/Training/MyProgram.java
+++ src/Training/MyProgram.java
@@ -6,14 +6,9 @@ class MyProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = b * b - 4 * a * c;
 
-        if (d < 0) {
-            System.out.println("0");
-        } else {
-            System.out.println(d > 0 ? "2" : "1");
-        }
+        System.out.println(a >= 21 ? "Да" : "Нет");
+
+        sc.close();
     }
 }
