95055ddc82ff9cbd55de2819e6c727fb3029db68
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-27 15:56:42 +0400

    training

---
+++ src/Training/MyProgram.java
@@ -0,0 +1,13 @@
+package Training;
+
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+
+        sc.close();
+    }
+}
