f52c5b11b8be15632f960f923b99d34f35db806f
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-12-27 16:04:23 +0400

    training

--- src/Training/MyProgram.java
+++ src/Training/MyProgram.java
@@ -2,17 +2,18 @@ package Training;
 
 import java.util.Scanner;
 
-class MyProgram{
-    public static void main(String[] args){
+class MyProgram {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if (a == 0 && b == 0) {
-            System.out.println("inf");
-        } else {
-            System.out.println(a != 0 ? "1" : "0");
-        }
+        int c = sc.nextInt();
+        int d = b * b - 4 * a * c;
 
-        sc.close();
+        if (d < 0) {
+            System.out.println("0");
+        } else {
+            System.out.println(d > 0 ? "2" : "1");
+        }
     }
 }
