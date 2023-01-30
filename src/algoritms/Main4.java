cf164a06ae7a953d54d8e45bda0b2fe0fac9edf5
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-01-30 15:49:07 +0400

    Сортировка пузырьком

--- src/algoritms/Main4.java
+++ src/algoritms/Main4.java
@@ -1,19 +1,24 @@
 package algoritms;
 
-public class Main3 {
+import java.util.Arrays;
+
+public class Main4 {
     public static void main(String[] args) {
-        int[] array = new int[]{64, 32, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};  //Сортировка пузырьком
+        int[] array = new int[] {3, 6, 2, 77, 34, 22};
         int rsl;
         boolean isSorted = false;
 
         while (!isSorted) {
+            isSorted = true;
             for (int i = 1; i < array.length; i++) {
                 if (array[i] < array[i - 1]) {
                     rsl = array[i];
                     array[i] = array[i - 1];
                     array[i - 1] = rsl;
+                    isSorted = false;
                 }
             }
         }
+        System.out.println(Arrays.toString(array));
     }
 }
