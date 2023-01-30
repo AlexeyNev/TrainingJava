7d1e956fc84b08cf62c74f4cdac48538170e6009
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-01-30 16:08:36 +0400

    Сортировка выбором

--- src/algoritms/Main.java
+++ src/algoritms/Main.java
@@ -1,15 +1,31 @@
 package algoritms;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[] {64, 32, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};
-        int minValue = array[0];
+        int[] array = new int[] {64, 32, 73, 41, 31};
 
-        for (int i = 0; i < array.length; i++) {
+        for (int step = 0; step < array.length; step++) {
+            System.out.println(Arrays.toString(array));
+            int index = min(array, step);
+
+            int tmp = array[step];
+            array[step] = array[index];
+            array[index] = tmp;
+        }
+    }
+
+    private static int min(int[] array, int start) {
+        int minIndex = start;
+        int minValue = array[start];
+
+        for (int i = start; i < array.length; i++) {
             if (array[i] < minValue) {
                 minValue = array[i];
+                minIndex = i;
             }
         }
-        System.out.println(minValue);
+        return minIndex;
     }
 }
---
+++ src/algoritms/Main5.java
@@ -0,0 +1,10 @@
+package algoritms;
+
+import java.io.File;
+import java.util.List;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+    }
+}
