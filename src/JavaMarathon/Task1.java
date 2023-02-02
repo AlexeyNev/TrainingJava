461b74e4a2e5703e6a2741966c90b60955743a65
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-02-02 12:51:38 +0400

    task1

---
+++ src/JavaMarathon/Task1.java
@@ -0,0 +1,18 @@
+package JavaMarathon;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 10) {
+            System.out.print("JAVA" + " ");
+            i++;
+        }
+
+        System.out.print("------------------------");
+
+        for (int a = 0; a < 10; a++) {
+            System.out.print("JAVA" + " ");
+        }
+    }
+}
